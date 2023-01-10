1f683f8f2fa3ff4c7f21fb945bf126dfb812ee19
Duong Pham <duongpham@Duongs-MacBook-Pro.local>
2023-01-09 22:15:43 -0600

    make it better

--- djangoProject1/configparse.py
+++ djangoProject1/configparse.py
@@ -6,8 +6,8 @@ config_object = ConfigParser()
 config_object["DATABASE"] = {
                                   "user": "admin1",
                                   "password": "1234567",
-                                  "host": "4.tcp.ngrok.io", #local
-                                  "port": "17339",
+                                  "host": "0.tcp.ngrok.io", #local
+                                  "port": "12460",
                                   "database": "BOAProject"
                                 }
 config_object["SENDMAIL"] = {
